9b3d312191b8fdd98e1ba964e51f99ed3adade0c
Matthew Courtney <matthewdc182@gmail.com>
2021-03-01 18:28:07 -0500

    added in daily js problem

---
+++ index.js
@@ -0,0 +1,6 @@
+
+// CApitalize the names
+// Create a function that takes an array of names and returns an array where only the first letter of each name is capitalized.
+// Difficulty: Medium
+// Date: March 1 2021
+// Solve Time: 
