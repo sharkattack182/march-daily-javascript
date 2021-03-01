1f9e6caa833953d15c29e01a7b6749c7ec7fbb27
Matthew Courtney <matthewdc182@gmail.com>
2021-03-01 18:32:51 -0500

    finished in about 4 minutes

--- index.js
+++ index.js
@@ -3,4 +3,20 @@
 // Create a function that takes an array of names and returns an array where only the first letter of each name is capitalized.
 // Difficulty: Medium
 // Date: March 1 2021
-// Solve Time: 
+// Solve Time: 4 minutes
+
+function capMe(arr) {
+    var newArr = []
+    arr.forEach(name => {
+        let capd = name[0].toUpperCase() + name.toLowerCase().substring(1);
+        newArr.push(capd);
+    });
+
+    console.log(newArr)
+}
+
+
+
+capMe(["mavis", "senaida", "letty"])
+capMe(["samuel", "MABELLE", "letitia", "meridith"])
+capMe(["Slyvia", "Kristal", "Sharilyn", "Calista"])
