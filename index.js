ebc9bc108b6604ce9db364c678e2b3d2b8395813
Matthew Courtney <matthewdc182@gmail.com>
2021-03-02 20:51:56 -0500

    added in the daily problem:

--- index.js
+++ index.js
@@ -5,18 +5,26 @@
 // Date: March 1 2021
 // Solve Time: 4 minutes
 
-function capMe(arr) {
-    var newArr = []
-    arr.forEach(name => {
-        let capd = name[0].toUpperCase() + name.toLowerCase().substring(1);
-        newArr.push(capd);
-    });
+// function capMe(arr) {
+//     var newArr = []
+//     arr.forEach(name => {
+//         let capd = name[0].toUpperCase() + name.toLowerCase().substring(1);
+//         newArr.push(capd);
+//     });
 
-    console.log(newArr)
-}
+//     console.log(newArr)
+// }
 
 
 
-capMe(["mavis", "senaida", "letty"])
-capMe(["samuel", "MABELLE", "letitia", "meridith"])
-capMe(["Slyvia", "Kristal", "Sharilyn", "Calista"])
+// capMe(["mavis", "senaida", "letty"])
+// capMe(["samuel", "MABELLE", "letitia", "meridith"])
+// capMe(["Slyvia", "Kristal", "Sharilyn", "Calista"])
+
+
+
+// Positive Count / Negative Sum
+// Create a function that takes an array of positive and negative numbers. Return an array where the first element is the count of positive numbers and the second element is the sum of negative numbers.
+// Difficulty: Medium
+// Date: March 2 2021
+// Solve Time: 
